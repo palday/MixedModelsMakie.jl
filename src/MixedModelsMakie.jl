fc4e03565ca8b97fe18150cd518e33ee0ae11dad
Douglas Bates <dmbates@gmail.com>
2021-04-30 15:49:28 -0500

    Add a caterpillar plot and RanefInfo struct

--- src/MixedModelsMakie.jl
+++ src/MixedModelsMakie.jl
@@ -1,13 +1,19 @@
 module MixedModelsMakie
     using AbstractPlotting
+    using LinearAlgebra
     using MixedModels
 
     export
         CoefByGroup,
+        RanefInfo,
 
+        caterpillar,
+        caterpillar!,
+        ranefinfo,
         shrinkage,
         shrinkageplot
 
     include("shrinkage.jl")
+    include("caterpillar.jl")
 
 end
