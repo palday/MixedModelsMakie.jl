458b799dc0e91c5122d3c774173dd4ffb2da6e8d
Douglas Bates <dmbates@gmail.com>
2021-06-02 13:50:08 -0500

    Return a Tuple from simplelinreg (#16)
    * Return a Tuple from simplelinreg
    
    * add tests
    
    * Bump minor version b/c breaking change
    
    Co-authored-by: Phillip Alday <me@phillipalday.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using MixedModelsMakie
+using Random # we don't depend on exact PRNG vals, so no need for StableRNGs
 using Test
 
-@testset "There are no tests" begin
+@testset "There are no graphical tests" begin
     @test true
+end
+
+@testset "Simple linear regression" begin
+    a, b = 1, 2
+    x = collect(1:10)
+    y = randn(MersenneTwister(42), 10) * 0.1
+    @. y += a + b * x
+    result = simplelinreg(x, y)
+    @test result isa Tuple
+    @test a ≈ result[1] atol=0.05
+    @test b ≈ result[2] atol=0.05
 end
