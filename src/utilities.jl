7d6972d3b291fecf33550aec05b4bd098e5f4e15
Phillip Alday <me@phillipalday.com>
2021-09-07 10:48:38 +0200

    move confint_table to utilities

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,4 +1,48 @@
 """
+    confint_table(x::StatsBase.StatisticalModel, level=0.95)
+    confint_table(x::MixedModelBootstrap, level=0.95)
+
+Return a DataFrame of coefficient names, point estimates and confidence intervals.
+
+`level` specifies the confidence level, e.g. `0.95` for 95% confidence intervals.
+
+For `MixedModels`, the intervals are computed using the standard errors and the asymptotic
+Wald approximation (e.g. est±1.96*se for 95% intervals). For `MixedModelBootstrap`, the intervals
+are computed using `shortestcovint`, and the point estimate re-computed as the mean of the bootstrap values.
+
+The returned table has the following columns:
+- `coefname`: the names of the coefficients
+- `estimate`: the point estimates
+- `lower`: the lower edge of the confidence interval
+- `upper`: the upper edge of the confidence interval
+
+"""
+function confint_table(x::StatsBase.StatisticalModel, level=0.95)
+    # taking from the lower tail
+    semultiple = zquantile((1 - level) / 2)
+    se = stderror(x)
+
+    return DataFrame(;
+        coefname=coefnames(x),
+        estimate=coef(x),
+        lower=coef(x) + semultiple * se,
+        upper=coef(x) - semultiple * se,
+    )
+end
+
+function confint_table(x::MixedModelBootstrap, level=0.95)
+    df = transform!(
+        select!(DataFrame(x.β), Not(:iter)), :coefname => ByRow(string) => :coefname
+    )
+    return combine(
+        groupby(df, :coefname),
+        :β => mean => :estimate,
+        :β => NamedTuple{(:lower, :upper)} ∘ shortestcovint => [:lower, :upper],
+    )
+end
+
+
+"""
     ppoints(n::Integer)
 
 Return a sequence of `n` equally-spaced points in the interval (0, 1) - so-called "probability points"
