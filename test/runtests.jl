b3ebb36d2f00bcb3ec885470c56273be60a79903
Phillip Alday <me@phillipalday.com>
2022-06-24 15:48:03 -0500

    oops

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,7 +30,7 @@ b1 = parametricbootstrap(MersenneTwister(42), 100, m1)
 g1 = fit(MixedModel,
          @formula(r2 ~ 1 + anger + gender + btype + situ +
                        (1 | subj) + (1 + gender | item)),
-         verbagg,
+         MixedModels.dataset(:verbagg),
          Bernoulli(); progress)
 
 @testset "[qq]caterpillar" begin
