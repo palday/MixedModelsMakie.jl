7f91fa21ae8faeaff4d35a7c76e185ab0c9c2263
Douglas Bates <dmbates@gmail.com>
2022-11-01 12:35:01 -0500

    Add profiledensity, update zetaplot

--- src/profile.jl
+++ src/profile.jl
@@ -2,48 +2,49 @@ function zetaplot(
     pr::MixedModelProfile;
     absv::Bool=true,
     coverage=[0.5, 0.8, 0.9, 0.95, 0.99],
-    figure=(; resolution=(1000,600))
+    resolution=(1200,600),
 )
     cutoffs = sqrt.(quantile(Chisq(1), coverage))
-    yaxs = absv ? :ζ => abs => "|ζ|" : :ζ
-    (; prtbl, δ, fecnames) = pr
-    draw(
-        data(
-            (;
-            δ = repeat(collect(δ); outer=length(fecnames)),
-            ζ = prtbl.ζ,
-            cnames = repeat(fecnames, inner=length(δ)),
-            ),
-        ) *
-        mapping(:δ, yaxs, color=:cnames => "Coefficient") *
-        visual(Lines);
-        figure,
-    )
+    zbd = 1.05 * maximum(cutoffs)
+    f = Figure(; resolution)
+    (; fwd, rev) = pr
+    ylabel = absv ? "|ζ|" : "ζ"
+    for (i, p) in enumerate(keys(fwd))
+        rp, fw = rev[p], fwd[p]
+        ax = Axis(f[1, i]; xlabel=string(p), ylabel)
+        knts = knots(rp)
+        intvl = rp(max(-zbd, first(knts)))..rp(min(zbd, last(knts)))
+        lines!(ax, intvl, (absv ? abs : identity) ∘ fw)
+        if absv
+            sgncp = repeat(cutoffs; inner=2) .* repeat([-1, 1]; outer=length(cutoffs))
+            linesegments!(
+                rp.(clamp.(sgncp, first(knts), last(knts))),
+                abs.(sgncp),
+            )
+        end
+    end
+    f
 end
 
-function deltadensity(
+function profiledensity(
     pr::MixedModelProfile;
-    npts::Integer=129,
-    figure=(; resolution=(1000, 600)),
+    resolution=(1200, 500),
+    zbd=3,
 )
-    (; prtbl, δ, fecnames) = pr
-    ord, nat = BSplineOrder(4), Natural()
-    splines = [interpolate(ζ, δ, ord, nat) for ζ in eachcol(reshape(prtbl.ζ, length(δ), :))]
-    xpat = range(first(δ), last(δ); length=npts)
-    xv = repeat(xpat; outer=length(fecnames))
-    cnames = repeat(fecnames; inner=length(xpat))
-    dv = @. exp(-abs2(xv) / 2) / inv(sqrt(2π)) 
-    dens = dv .* foldl(vcat, map(s -> (Derivative(1) * s).(xpat), splines))
-    draw( 
-        data((; xv, dens, cnames)) *
-        mapping(
-            :xv => "δ",
-            :dens => "Effective probability density";
-            color=:cnames => "Coefficient",
-        ) * 
-        visual(Lines);
-        figure,
-    )
+    (; fwd, rev) = pr
+    f = Figure(; resolution)
+    for (i, p) in enumerate(keys(fwd))
+        rp, fw = rev[p], fwd[p]
+        ax = Axis(f[1, i]; xlabel=string(p), ylabel="pdf")
+        knts = knots(rp)
+        intvl = rp(max(-zbd, first(knts)))..rp(min(zbd, last(knts)))
+        lines!(
+            ax,
+            intvl,
+            x -> pdf(Normal(), fw(x)) * (Derivative(1) * fw)(x) 
+        )
+    end
+    f
 end
 
 function zetatraces!(ax::Axis, pr::MixedModelProfile, i, j)
@@ -67,3 +68,27 @@ function zetatraceplot(pr::MixedModelProfile; figure=(; resolution=(800,800)))
     return f
 end
 
+"""
+    _signedavgdelta(x, y)
+
+Return the signed average and half-difference of acos(x) / π and acos(y) / π
+
+Because of the range of
+"""
+function _signedavgdelta(x, y)
+    acpix, acpiy = acos.((x, y)) ./ π
+    d = (acpix - acpiy) / 2
+    return sign(d) * (acpix + acpiy) / 2, abs(d)
+end
+
+"""
+    zetaijspl(pr, i, j)
+
+Return an interpolation spline for the βj trace on βi in the zeta scale.
+"""
+function zetaijspline!(pr::MixedModelProfile, i, j)
+    (; prtbl, δ, fwd, rev) = pr
+    iinds = axes(δ, 1) .+ (i - 1) * length(δ)  # row indices for i'th trace in prtbl
+    zetavalsj = fwd[j].(getindex.(view(prtbl.β, iinds), j))
+    return interpolate!(rev[i], zetavalsj)
+end
