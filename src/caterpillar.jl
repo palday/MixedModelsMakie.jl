7e7052b1a351eb5c8b4392394caf8f491cac2b0a
Phillip Alday <phillip.alday@beacon.bio>
2023-06-28 10:36:46 -0400

    Add cols kwarg to caterpillar

--- src/caterpillar.jl
+++ src/caterpillar.jl
@@ -77,7 +77,10 @@ function ranefinfotable(ri::RanefInfo)
 end
 
 """
-    caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo; orderby=1)
+    caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo;
+                orderby=1, cols=nothing)
+    caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, m::LinearMixedModel,
+                 gf::Symbol=first(fnames(m)); orderby=1, cols=nothing)
 
 Add Axes of a caterpillar plot from `r` to `f`.
 
@@ -86,14 +89,24 @@ of `r.ranef`, usually the `(Intercept)` random effects.
 Setting `orderby=nothing` will disable sorting, i.e. return the levels in the
 order they are stored in.
 
+The display can be restricted to a subset of random effects associated with a grouping variable by
+specifying `cols`, either by indices or term names.
+
 !!! note
     Even when not sorting the levels, they might have already been sorted during
     model matrix construction. If you want impose a particular ordering on the
     levels, then you must sort the relevant fields in the `RanefInfo` object before
     calling `caterpillar!`.
+
+!!! note
+    `orderby` is the ``n``th column of the columns specified by `cols`.
 """
-function caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo; orderby=1)
-    rr = r.ranef
+function caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo;
+                      orderby=1, cols=nothing)
+    cols = something(cols, axes(r.cnames, 1))
+    cols = _cols_to_idx(r, cols)
+    rr = view(r.ranef, :, cols)
+    sd = view(r.stddev, :, cols)
     y = axes(rr, 1)
     ord = isnothing(orderby) ? y : sortperm(view(rr, :, orderby))
     cn = r.cnames
@@ -102,7 +115,7 @@ function caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo;
     for (j, ax) in enumerate(axs)
         xvals = view(rr, ord, j)
         scatter!(ax, xvals, y; color=(:red, 0.2))
-        errorbars!(ax, xvals, y, 1.960 * view(r.stddev, ord, j); direction=:x)
+        errorbars!(ax, xvals, y, 1.960 * view(sd, ord, j); direction=:x)
         ax.xlabel = cn[j]
         ax.yticks = y
         j > 1 && hideydecorations!(ax; grid=false)
@@ -111,27 +124,52 @@ function caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo;
     return f
 end
 
+function caterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, m::MixedModel,
+                      gf::Symbol=first(fnames(m)); kwargs...)
+    return caterpillar!(f, ranefinfo(m, gf); kwargs...)
+end
+
 """
-    caterpillar(m::LinearMixedModel, gf::Symbol)
+    caterpillar(m::LinearMixedModel, gf::Symbol; orderby=1, cols=nothing)
 
 Returns a `Figure` of a "caterpillar plot" of the random-effects means and prediction intervals
 
 A "caterpillar plot" is a horizontal error-bar plot of conditional means and standard deviations
 of the random effects.
+
+The order of the levels on the vertical axes is increasing `orderby` column
+of `r.ranef`, usually the `(Intercept)` random effects.
+Setting `orderby=nothing` will disable sorting, i.e. return the levels in the
+order they are stored in.
+
+The display can be restricted to a subset of random effects associated with a grouping variable by
+specifying `cols`, either by indices or term names.
+
+!!! note
+    `orderby` is the ``n``th column of the columns specified by `cols`.
 """
-function caterpillar(m::MixedModel, gf::Symbol=first(fnames(m)))
-    return caterpillar!(Figure(; resolution=(1000, 800)), ranefinfo(m)[gf])
+function caterpillar(m::MixedModel, gf::Symbol=first(fnames(m)); kwargs...)
+    return caterpillar!(Figure(; resolution=(1000, 800)), m; kwargs...)
 end
 
 """
-    qqcaterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo; cols=axes(r.cnames, 1))
+    qqcaterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo; cols=nothing)
+    qqcaterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, m::LinearMixedModel,
+                   gf::Symbol=first(fnames(m)); cols=nothing)
 
 Update the figure with a caterpillar plot with the vertical axis on the Normal() quantile scale.
 
-The display can be restricted to a subset of random effects associated with a grouping variable by specifying `cols`, either by indices or term names.
+The display can be restricted to a subset of random effects associated with a grouping variable by
+specifying `cols`, either by indices or term names.
+
+The order of the levels on the vertical axes is increasing `orderby` column
+of `r.ranef`, usually the `(Intercept)` random effects.
+Setting `orderby=nothing` will disable sorting, i.e. return the levels in the
+order they are stored in.
 """
 function qqcaterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInfo;
-                        cols=axes(r.cnames, 1))
+                        cols=nothing)
+    cols = something(cols, axes(r.cnames, 1))
     cols = _cols_to_idx(r, cols)
     cn, rr = r.cnames, r.ranef
     y = zquantile.(ppoints(size(rr, 1)))
@@ -150,21 +188,25 @@ function qqcaterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, r::RanefInf
     return f
 end
 
+function qqcaterpillar!(f::Union{Makie.FigureLike,Makie.GridLayout}, m::MixedModel,
+                        gf::Symbol=first(fnames(m)); kwargs...)
+    return qqcaterpillar!(f, ranefinfo(m, gf); kwargs...)
+end
+
+"""
+    qqcaterpillar(m::LinearMixedModel, gf::Symbol=first(fnames(m)); cols=nothing, orderby=1)
+
+Returns a `Figure` of a "qq-caterpillar plot" of the random-effects means and prediction intervals.
+
+The display can be restricted to a subset of random effects associated with a grouping variable by
+specifying `cols`, either by indices or term names.
+"""
+function qqcaterpillar(m::MixedModel, gf::Symbol=first(fnames(m)); kwargs...)
+    return qqcaterpillar!(Figure(; resolution=(1000, 800)), m, gf; kwargs...)
+end
+
 _cols_to_idx(r, cols) = cols
 _cols_to_idx(r, cols::AbstractVector{<:Symbol}) = _cols_to_idx(r, string.(cols))
 function _cols_to_idx(r, cols::Vector{<:AbstractString})
     return [i for (i, c) in enumerate(r.cnames) if c in cols]
-end
-
-"""
-    qqcaterpillar(m::LinearMixedModel, gf::Symbol=first(fnames(m)); cols=nothing)
-
-Returns a `Figure` of a "qq-caterpillar plot" of the random-effects means and prediction intervals.
-
-The display can be restricted to a subset of random effects associated with a grouping variable by specifying `cols`, either by indices or term names.
-"""
-function qqcaterpillar(m::MixedModel, gf::Symbol=first(fnames(m)); cols=nothing)
-    reinfo = ranefinfo(m, gf)
-    cols = something(cols, axes(reinfo.cnames, 1))
-    return qqcaterpillar!(Figure(; resolution=(1000, 800)), reinfo; cols=cols)
 end
