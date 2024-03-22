236e210cfc11584ca833bf06314e7cbab3d46016
Phillip Alday <me@phillipalday.com>
2024-03-21 20:04:46 -0500

    coefplot

--- src/coefplot.jl
+++ src/coefplot.jl
@@ -1,30 +1,31 @@
 """
-    coefplot(x::MixedModel;
-             conf_level=0.95, vline_at_zero=true, show_intercept=true, attributes...)
-    coefplot(x::MixedModelBootstrap;
-             conf_level=0.95, vline_at_zero=true, show_intercept=true, attributes...)
+    coefplot(x::Union{MixedModel,MixedModelBootstrap}; kwargs...)::Figure
+    coefplot!(fig::$(Indexable), x::Union{MixedModel,MixedModelBootstrap}; 
+              kwargs...)
+    coefplot!(ax::Axis, Union{MixedModel,MixedModelBootstrap};
+              conf_level=0.95, vline_at_zero=true, show_intercept=true, attributes...)
 
 Create a coefficient plot of the fixed-effects and associated confidence intervals.
 
-!!! note
-    This functionality is implemented using [Makie recipes](https://makie.juliaplots.org/v0.15.0/recipes.html)
-    and thus there are also additional auto-generated methods for `coefplot` and `coefplot!` that may be useful
-    when constructing more complex figures.
+`attributes` are passed onto `scatter!` and `errorbars!`.
+
+The mutating methods return the original object.
 """
-function coefplot(x::Union{MixedModel,MixedModelBootstrap};
-                  conf_level=0.95,
-                  vline_at_zero=true,
-                  show_intercept=true,
-                  attributes...)
+function coefplot(x::Union{MixedModel,MixedModelBootstrap}; show_intercept=true, kwargs...)
     # need to guarantee a min height of 150
     fig = Figure(; size=(640, max(150, 75 * _npreds(x; show_intercept))))
-    ax = Axis(fig[1, 1])
-    coefplot!(ax, x; conf_level, vline_at_zero, show_intercept, attributes...)
-    return Makie.FigureAxis(fig, ax)
+    coefplot!(fig, x; kwargs...)
+    return fig
 end
 
+"""$(@doc coefplot)"""
+function coefplot!(fig::Indexable, x::Union{MixedModel,MixedModelBootstrap}; kwargs...)
+    ax = Axis(fig[1, 1])
+    coefplot!(ax, x; kwargs...)
+    return fig
+end
 
-# Indexable
+"""$(@doc coefplot)"""
 function coefplot!(ax::Axis, x::Union{MixedModel,MixedModelBootstrap};
                    conf_level=0.95,
                    vline_at_zero=true,
@@ -39,7 +40,8 @@ function coefplot!(ax::Axis, x::Union{MixedModel,MixedModelBootstrap};
     attributes = merge((;xlabel), attributes)
 
     scatter!(ax, xvals, y; attributes...)
-    errorbars!(ax, xvals, y, xvals .- ci.lower, ci.upper .- xvals; direction=:x)
+    errorbars!(ax, xvals, y, xvals .- ci.lower, ci.upper .- xvals; 
+               direction=:x, attributes...)
     vline_at_zero && vlines!(ax, 0; color=(:black, 0.75), linestyle=:dash)
 
     reset_limits!(ax)
