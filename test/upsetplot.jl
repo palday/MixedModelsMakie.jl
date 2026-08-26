129aa441c5316353e0dd983ec32e03323d51cd09
Phillip Alday <me@phillipalday.com>
2026-08-26 12:42:31 +0200

    moar tests

--- test/upsetplot.jl
+++ test/upsetplot.jl
@@ -53,6 +53,49 @@ end
            match_mode = :any,
            @test_throws ArgumentError upsetplot(kb07data; cols=Not(:rt_trunc)))
 
+# --- Layout options ---
+
+# Vertical orientation
+f = upsetplot(m2; orientation=:vertical)
+@test save(joinpath(OUTDIR, "upset_kb07_vertical.png"), f)
+
+# Vertical with intersection on the right
+f = upsetplot(m2; orientation=:vertical, intersection_pos=:right)
+@test save(joinpath(OUTDIR, "upset_kb07_vertical_intright.png"), f)
+
+# Vertical with set-size bars on the bottom (x-axis labels move to top)
+f = upsetplot(m2; orientation=:vertical, setsize_pos=:bottom)
+@test save(joinpath(OUTDIR, "upset_kb07_vertical_setbot.png"), f)
+
+# Horizontal with intersection on the bottom
+f = upsetplot(m2; intersection_pos=:bottom)
+@test save(joinpath(OUTDIR, "upset_kb07_intbottom.png"), f)
+
+# Horizontal with set-size bars on the right
+f = upsetplot(m2; setsize_pos=:right)
+@test save(joinpath(OUTDIR, "upset_kb07_setright.png"), f)
+
+# Horizontal with both flipped
+f = upsetplot(m2; intersection_pos=:bottom, setsize_pos=:right)
+@test save(joinpath(OUTDIR, "upset_kb07_intbottom_setright.png"), f)
+
+# No set-size bar chart
+f = upsetplot(m2; show_setsize=false)
+@test save(joinpath(OUTDIR, "upset_kb07_nosetsize.png"), f)
+
+# Vertical without set-size
+f = upsetplot(m2; orientation=:vertical, show_setsize=false)
+@test save(joinpath(OUTDIR, "upset_kb07_vertical_nosetsize.png"), f)
+
+# Error: invalid orientation
+@test_throws ArgumentError upsetplot(m2; orientation=:diagonal)
+
+# Error: wrong position for orientation
+@test_throws ArgumentError upsetplot(m2; intersection_pos=:left)
+@test_throws ArgumentError upsetplot(m2; orientation=:vertical, intersection_pos=:top)
+@test_throws ArgumentError upsetplot(m2; setsize_pos=:top)
+@test_throws ArgumentError upsetplot(m2; orientation=:vertical, setsize_pos=:left)
+
 @testset "upsettable" begin
     ut = upsettable(g1)
     @test names(ut)[1:3] == ["cell", "degree", "count"]
